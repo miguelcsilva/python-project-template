b26b7d51c57d9ceb003c5409ea9caa98cc8ec435
Miguel Silva <miguelcesilva@gmail.com>
2024-09-22 19:53:59 +0100

    Fix rename bug

--- rename.sh
+++ rename.sh
@@ -8,7 +8,7 @@ then
   exit 1;
 fi
 
-find project_name -type f -exec sed -i "s/project_name/$new_project_name/g" {} \;
+find src -type f -exec sed -i "s/project_name/$new_project_name/g" {} \;
 find tests -type f -exec sed -i "s/project_name/$new_project_name/g" {} \;
 sed -i "s/project_name/$new_project_name/g" pyproject.toml;
-mv project_name $new_project_name
+mv src/project_name src/$new_project_name
