b805be2a5734677920ef43a2014dfaa76b4b1b75
Miguel Silva <miguelcesilva@gmail.com>
2024-12-11 15:19:59 +0000

    Update settings

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -1,35 +1,66 @@
-from enum import StrEnum
+import os
+from enum import StrEnum, auto
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class LogLevel(StrEnum):
-    CRITICAL = "critical"
-    ERROR = "error"
-    WARNING = "warning"
-    INFO = "info"
-    DEBUG = "debug"
+    CRITICAL = auto()
+    ERROR = auto()
+    WARNING = auto()
+    INFO = auto()
+    DEBUG = auto()
 
 
 class LogRenderer(StrEnum):
-    TEXT = "text"
-    JSON = "json"
+    TEXT = auto()
+    JSON = auto()
 
 
 class Environment(StrEnum):
-    LOCAL = "local"
-    DEFAULT = "default"
+    LOCAL = auto()
+    TEST = auto()
+    DEFAULT = auto()
 
 
 class _Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        frozen=True,
     )
-
     ENVIRONMENT: Environment = Environment.DEFAULT
     LOG_LEVEL: LogLevel = LogLevel.INFO
     LOG_RENDERER: LogRenderer = LogRenderer.TEXT
     THIRD_PARTY_LOG_LEVEL: LogLevel = LogLevel.WARNING
 
 
-SETTINGS = _Settings()
+class _LocalSettings(_Settings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        frozen=True,
+    )
+    ENVIRONMENT: Environment = Environment.LOCAL
+
+
+class _TestSettings(_Settings):
+    model_config = SettingsConfigDict(
+        frozen=True,
+    )
+    ENVIRONMENT: Environment = Environment.TEST
+
+
+def _get_settings() -> _Settings:
+    environment_str = os.environ.get("ENVIRONMENT", "default")
+    environment = (
+        Environment(environment_str)
+        if environment_str in Environment
+        else Environment.DEFAULT
+    )
+    mapping = {
+        Environment.DEFAULT: _Settings,
+        Environment.LOCAL: _LocalSettings,
+        Environment.TEST: _TestSettings,
+    }
+    return mapping[environment](ENVIRONMENT=environment)
+
+
+SETTINGS = _get_settings()
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,58 +2,31 @@ import pytest
 
 from project_name.settings import (
     Environment,
-    LogLevel,
-    LogRenderer,
+    _get_settings,
+    _LocalSettings,
     _Settings,
+    _TestSettings,
 )
 
 
 @pytest.mark.parametrize(
-    argnames=["name", "value", "expected"],
+    argnames=["environment", "additional_attributes", "expected_settings"],
     argvalues=[
-        ["ENVIRONMENT", "local", Environment.LOCAL],
-        ["ENVIRONMENT", "default", Environment.DEFAULT],
-        ["LOG_LEVEL", "debug", LogLevel.DEBUG],
-        ["LOG_LEVEL", "info", LogLevel.INFO],
-        ["LOG_LEVEL", "warning", LogLevel.WARNING],
-        ["LOG_LEVEL", "error", LogLevel.ERROR],
-        ["LOG_LEVEL", "critical", LogLevel.CRITICAL],
-        ["LOG_RENDERER", "text", LogRenderer.TEXT],
-        ["LOG_RENDERER", "json", LogRenderer.JSON],
-        ["THIRD_PARTY_LOG_LEVEL", "debug", LogLevel.DEBUG],
-        ["THIRD_PARTY_LOG_LEVEL", "info", LogLevel.INFO],
-        ["THIRD_PARTY_LOG_LEVEL", "warning", LogLevel.WARNING],
-        ["THIRD_PARTY_LOG_LEVEL", "error", LogLevel.ERROR],
-        ["THIRD_PARTY_LOG_LEVEL", "critical", LogLevel.CRITICAL],
+        ["local", {}, _LocalSettings(ENVIRONMENT=Environment.LOCAL)],
+        ["test", {}, _TestSettings(ENVIRONMENT=Environment.TEST)],
+        ["default", {}, _Settings()],
+        [None, {}, _Settings()],
     ],
 )
-def test_settings_attribute_is_correctly_instantiated_when_provided(
+def test_get_settings_from_environment(
     monkeypatch: pytest.MonkeyPatch,
-    name: str,
-    value: str,
-    expected: str,
+    environment: str | None,
+    additional_attributes: dict[str, str],
+    expected_settings: _Settings,
 ) -> None:
-    monkeypatch.setenv(name=name, value=value)
-    settings = _Settings()
-    attribute = getattr(settings, name)
-    assert attribute is expected
-
-
-@pytest.mark.parametrize(
-    argnames=["name", "expected"],
-    argvalues=[
-        ["ENVIRONMENT", Environment.DEFAULT],
-        ["LOG_LEVEL", LogLevel.INFO],
-        ["LOG_RENDERER", LogRenderer.TEXT],
-        ["THIRD_PARTY_LOG_LEVEL", LogLevel.WARNING],
-    ],
-)
-def test_settings_attribute_has_correct_default_when_missing(
-    monkeypatch: pytest.MonkeyPatch,
-    name: str,
-    expected: str,
-) -> None:
-    monkeypatch.delattr(target=_Settings, name="model_config")
-    settings = _Settings()
-    attribute = getattr(settings, name)
-    assert attribute is expected
+    if environment:
+        monkeypatch.setenv("ENVIRONMENT", environment)
+    for name, value in additional_attributes.items():
+        monkeypatch.setenv(name, value)
+    settings = _get_settings()
+    assert settings == expected_settings
